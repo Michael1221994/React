d7ce54f184de208acbaf6db305f5e0c33deb0d0f
Michael1221994 <103842937+Michael1221994@users.noreply.github.com>
2026-03-19 22:15:19 +0300

    filtered

--- week-one-react/src/App.jsx
+++ week-one-react/src/App.jsx
@@ -3,10 +3,10 @@ import Student from './Student.jsx'
 function App() {
   return(
     <>
-      <Student name="Spongebob" age={30} IsStudent={True}/>
-      <Student name="Patrick" age={42} IsStudent={false}/>
-      <Student name="Squidward" age={50} IsStudent={false}/>
-      <Student name="Sandy" age={27} IsStudent={True}/>
+      <Student name="Spongebob"  IsStudent={True}/>
+      <Student name="Patrick"  IsStudent={false}/>
+      <Student name="Squidward"  IsStudent={false}/>
+      <Student name="Sandy"  IsStudent={True}/>
 
 
 
--- week-one-react/src/Student.jsx
+++ week-one-react/src/Student.jsx
@@ -3,20 +3,17 @@ function Student(props){
     return(
         <div className="Student">
             <p>Name: {props.name}</p>
-            <p>Age:{props.age}</p>
             <p>Student: {props.IsStudent ? "Yes" : "No"}</p>
         </div>
     );
 }
 Student.propTypes ={
     name: propTypes.string,
-    age: propTypes.number,
     IsStudent: propTypes.bool,
 }
 
 Student.defaultProps ={
     name: "Guest",
-    age: 0,
     IsStudent: false,
 }
 export default Student
